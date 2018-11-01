ca20a07166ab173291775648ddbde2ef25858287
Alseny Sylla <senyboy10@gmail.com>
2018-11-01 18:09:17 -0400

    Update hw3_cri.cpp
    added important headers and created the main function

--- hw3_cri.cpp
+++ hw3_cri.cpp
@@ -1,1 +1,40 @@
+/*
+	Alseny Sylla
+	Chen
 
+	Assignment 3
+*/
+#include <poll.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include   <netdb.h>
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <cstdlib>
+#include <stdlib.h>
+#include <list>
+#include <arpa/inet.h>
+#include <vector>
+#include <utility>
+#include <map>
+#include <string>
+#include <regex>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/select.h>      /* <===== */
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+  return EXIT_SUCCESS;
+}
