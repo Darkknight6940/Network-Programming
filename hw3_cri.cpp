3dfc88e25fc4179cf77306b81b23d8eb29770a94
qianjunc <42955890+qianjunc@users.noreply.github.com>
2018-11-01 20:53:45 -0400

    Update hw3_cri.cpp

--- hw3_cri.cpp
+++ hw3_cri.cpp
@@ -33,6 +33,92 @@
 
 using namespace std;
 
+class User{
+  public:
+    User(std::string username){
+      name = username;
+      isoperator = false;
+    }
+
+    const std::string get_name() const{
+      return name;
+    }
+
+    void makeOperator(){
+      isOperator = true;
+    }
+
+    bool is_operator(){
+      return isOperator;
+    }
+
+    ~User(){
+
+    }
+
+
+  private:
+    std::string name;
+    bool isOperator;
+
+};
+
+class Channel{
+  public:
+    Channel(std::string cname){
+      users = std::list<User>();
+      channel_name = cname;
+    }
+
+    std::string get_cname() const{
+      return channel_name;
+    }
+
+    void add_user(std::string username){
+      users.push_back(User(username));
+    }
+
+    int users_num(){
+      return users.size();
+    }
+
+	bool remove_user(std::string username){
+      std::list<User>::iterator it = std::find(users.begin(), users.end(), User(username));
+      if(it!=users.end()){
+        users.erase(it);
+        return true;
+      }
+
+
+      return false;
+    }
+	
+    bool user_in(std::string username){
+      std::list<User>::iterator it = std::find(users.begin(), users.end(), User(username));
+      if(it != users.end()){
+        return true;
+      }
+
+      return false;
+	}
+
+    const std::list<User>& get_users() const{
+      return users;
+    }
+
+    ~Channel(){
+
+    }
+
+  private:
+    std::list<User> users;
+    std::string channel_name;
+
+
+};
+
+
+
 //Commands or Classes to Implement:
 /*
 	Channel, USER, LIST, JOIN, PART, OPERATOR, KICK, PRIVMSG, QUIT
